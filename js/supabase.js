8b1fc289066a6c788d8630cb10f31c78f48fa6e7
Titik Koma Coffee <titikkomacoffee01@gmail.com>
2026-09-21 01:38:27 +0800

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -3,8 +3,8 @@
 // GANTI dengan URL & KEY milik Anda!
 // ============================================
 
-const SUPABASE_URL = 'https://dcvuzxuxwmcowcnaevgp.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRjdnV6eHV4d21jb3djbmFldmdwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk3MjcyNDIsImV4cCI6MjEwNTMwMzI0Mn0.svdPVapnSZ4fHTmO2mAt9UUmJKYxJM1Kuie9cjBg3OI';
+const SUPABASE_URL = 'https://wqotezgkxysopdvpxusz.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indxb3RlemdreHlzb3BkdnB4dXN6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg5MTY5NTIsImV4cCI6MjEwNDQ5Mjk1Mn0.FaqsMh8FcWPNNcT8wAa0nUMYsCFCxZOpsx82gjO14ZM';
 
 const { createClient } = supabase;
 const db = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
